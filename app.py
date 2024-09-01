c9b30c42b0c217197d4f3dc6c5f02d9ecaffc711
rohit <rohit.selvakumaran@gmail.com>
2024-09-01 16:25:27 +0530

    Initial Commit

---
+++ app.py
@@ -0,0 +1,56 @@
+from flask import redirect,url_for,render_template,request,Flask
+from models import db,Todo
+
+app=Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todos.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db.init_app(app)
+
+#@app._got_first_request
+#def create_tables():
+#    db.create_all()
+    
+with app.app_context():
+    db.create_all()
+
+@app.route('/')
+def index():
+    todos = Todo.query.all()
+    return render_template('index.html', todos=todos)
+
+@app.route('/add', methods=['POST'])
+def add_todo():
+    title = request.form.get('title')
+    if title:
+        new_todo = Todo(title=title)
+        db.session.add(new_todo)
+        db.session.commit()
+    return redirect(url_for('index'))
+
+@app.route('/delete/<int:todo_id>')
+def delete_todo(todo_id):
+    todo = Todo.query.get(todo_id)
+    if todo:
+        db.session.delete(todo)
+        db.session.commit()
+    return redirect(url_for('index'))
+#Own Function
+@app.route('/clear')
+def clear_all():
+    todo_lst=Todo.query.all()
+    for i in todo_lst:
+        if i.completed == True:
+            db.session.delete(i)
+    db.session.commit()
+    return redirect(url_for('index'))
+
+@app.route('/complete/<int:todo_id>')
+def complete_todo(todo_id):
+    todo = Todo.query.get(todo_id)
+    if todo:
+        todo.completed = not todo.completed
+        db.session.commit()
+    return redirect(url_for('index'))
+
+if __name__ == '__main__':
+    app.run(debug=True)
