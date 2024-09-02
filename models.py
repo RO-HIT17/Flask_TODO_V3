232a7c02bffe4b17e8e5398d831ac0cca5963c74
rohit <rohit.selvakumaran@gmail.com>
2024-09-02 19:54:08 +0530

    added and modified column names

--- models.py
+++ models.py
@@ -3,16 +3,16 @@ from flask_sqlalchemy import SQLAlchemy
 db=SQLAlchemy()
 
 class Todo(db.Model):
-    id=db.Column(db.Integer,primary_key=True)
+    todo_id=db.Column(db.Integer,primary_key=True)
     title=db.Column(db.String(200))
     completed=db.Column(db.Boolean,default=False)
-    #userid=db.Column(db.Integer,unique=True)
+    user_id=db.Column(db.Integer,unique=True)
     
     def __repr__(self):
         return f"<Todo {self.title}>"
     
 class User(db.Model):
-    id=db.Column(db.Integer,primary_key=True)
+    user_id=db.Column(db.Integer,primary_key=True)
     name=db.Column(db.String(20))
     email = db.Column(db.String(120),unique=True)
     password= db.Column(db.String(20))
